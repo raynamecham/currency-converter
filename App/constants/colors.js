db73698af467e21834f409c52c2150dd39878af4
raynamecham <rayna.mecham@gmail.com>
2021-09-07 13:53:12 -0600

    worked on conversioninput, home

--- App/constants/colors.js
+++ App/constants/colors.js
@@ -1,5 +1,7 @@
 export default {
-  text: "#343434",
-  border: "#E2E2E2",
-  blue: "#4F6d7a",
+	text: '#343434',
+	border: '#E2E2E2',
+	blue: '#4F6d7a',
+	white: '#FFFFFF',
+	textLight: '#797979',
 };
